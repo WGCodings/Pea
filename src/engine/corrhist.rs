d541752e286ca3bcec4987ddd723813ca3cca220
warre <s0355721@gmail.com>
2026-06-17 12:25:27 +0200

    scale to max 64cp bench 3622272

--- src/engine/corrhist.rs
+++ src/engine/corrhist.rs
@@ -6,7 +6,7 @@ use crate::engine::hash::Hash;
 const SIZE: usize = 16384;
 const GRAIN: i32 = 256;
 const SCALE: i32 = 256;
-const MAX: i32 = 8192; // max correction is thus 32cp TODO experiment with larger MAX
+const MAX: i32 = 64*SCALE; // max correction is thus 32cp TODO experiment with larger MAX
 
 // Key can be different for other kinds of corrhist tables
 pub trait CorrHistKey {
@@ -56,7 +56,7 @@ impl<K: CorrHistKey> CorrectionHistoryTable<K> {
     // If key matches, correct raw eval with correction
     pub fn correct_evaluation(&self, pos: &Chess, raw_eval: i32) -> i32 {
         let entry = self.table[usize::from(pos.turn())][K::key(pos)];
-        raw_eval + 0*entry / GRAIN
+        raw_eval + entry / GRAIN
     }
 }
 
