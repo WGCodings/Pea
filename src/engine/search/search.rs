7c13abeed98f80d0781ae306fdcb2ed1198fa958
warre <s0355721@gmail.com>
2026-07-17 21:15:06 +0200

    fiddle with params try 5 bench 1374840

--- src/engine/search/search.rs
+++ src/engine/search/search.rs
@@ -369,7 +369,7 @@ pub fn negamax(
             ctx.stack.moves[ply] = None;
 
             if probcut_score >= probcut_beta {
-                tt_store(hash, ctx, depth - 4, probcut_beta, raw_eval, Bound::Lower, Some(mv), ply);
+                tt_store(hash, ctx, depth - 3, probcut_beta, raw_eval, Bound::Lower, Some(mv), ply);
                 return probcut_beta;
             }
         }
